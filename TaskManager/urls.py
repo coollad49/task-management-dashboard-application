be441dec2cc7a83d77227b07a6ae862a365ca8d8
Daniel <daniellucasadebayo@gmail.com>
2024-07-29 22:39:23 +0100

    config

--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from taskapp.views import login_view, logout_view, register
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,5 @@ urlpatterns = [
     path('logout', logout_view, name="logout"),
     path('register', register, name="register"),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
